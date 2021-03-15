c3a732ef1b637895a630c63f2f2dd26986112f52
mohamedZemroun <70947314+mohamedZemroun@users.noreply.github.com>
2021-03-15 12:53:33 +0100

    Add test

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/hello/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!\n'
 
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')     # open for everyone
